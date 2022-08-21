20c82ba388201250bcab7d7e82093eba5afec9d5
Yuvraj Kolambe <ykolambe123@gmail.com>
2022-08-21 22:23:16 +0400

    initial commit

---
+++ client/tax-alpha-v1/src/components/Header.js
@@ -0,0 +1,41 @@
+import {React} from 'react'
+import logo from '../logo.svg';
+import '../css/Header.css';
+import TextField from "@mui/material/TextField";
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header__left">
+            <img className="header__logo" src={logo} alt="" />
+            <h3>Services</h3>
+            <ArrowDropDownIcon className="arrow"/>
+        </div>
+        <div>
+            <h1>     </h1>
+        </div>
+      <div className="header__middle">
+        <TextField
+          id="outlined-basic"
+          variant="outlined"
+          fullWidth
+          label="Search"
+        />
+      </div>
+        <div className="header__right">
+            <div className='navmenu'>
+                <h3>GST</h3>
+                <h3>Notices</h3>
+                <h3>FAQ</h3>
+                <h3>Contact Us</h3>
+                <h3>Pricing</h3>
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ client/tax-alpha-v1/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header.js';
+
+const Home = () => {
+  return (
+    <div>
+        <div className="header">
+            <Header />
+        </div>
+    </div>
+  )
+}
+
+export default Home
