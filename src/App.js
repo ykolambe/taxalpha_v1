e83b4cb5a1df219358dbed384a090d455479fa5d
hack-dee <db671996@gmail.com>
2024-06-25 21:25:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -45,6 +45,14 @@ function App() {
 
           </Routes>
         </ScrollToTop>
+        <a
+        href="https://wa.me/2348100000000"
+        className="whatsapp_float"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="fa fa-whatsapp whatsapp-icon"></i>
+      </a>
       </Router>
 
   );
